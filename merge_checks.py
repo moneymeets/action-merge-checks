017e289b3d1d51448fd69baa980aa2d30e26d5f3
Steffen Ohrendorf <steffen.ohrendorf@moneymeets.com>
2021-04-29 09:00:00 +0200

    refactor(action): split subprocess calls into separate functions

--- merge_checks.py
+++ merge_checks.py
@@ -1,25 +1,46 @@
 import os
 import subprocess
 import sys
+from typing import Tuple
+
+
+def fetch_head_only(base_ref: str):
+    print(f"Checking out {base_ref}...")
+    subprocess.run(f"git fetch --depth=1 origin {base_ref}", check=True, shell=True)
+
+
+def fetch_full_history():
+    print("Getting commit list...")
+    subprocess.run("git fetch --unshallow", check=True, shell=True)
+
+
+def get_base_revision(base_ref: str) -> str:
+    return subprocess.run(
+        f"git rev-parse origin/{base_ref}", check=True, shell=True, capture_output=True, text=True).stdout.strip()
+
+
+def get_subject_markers(head_hash: str, base_hash: str) -> Tuple[str, ...]:
+    log = subprocess.run(f"git log --pretty='format:%s' {base_hash}..{head_hash}",
+                         check=True, shell=True, capture_output=True, text=True)
+    return tuple(line.split(maxsplit=1)[0] for line in log.stdout.splitlines())
+
+
+def has_merge_commits(head_hash: str, base_hash: str) -> bool:
+    parents = subprocess.run(f"git log --pretty=%p {base_hash}..{head_hash}",
+                             check=True, shell=True, capture_output=True, text=True)
+    return any(len(line.split(maxsplit=1)) > 1 for line in parents.stdout.splitlines())
 
 
 def main(head_hash: str, base_ref: str) -> int:
-    print(f"Checking out {base_ref}...")
-    subprocess.run(f"git fetch --depth=1 origin {base_ref}", check=True, shell=True)
-    base_revision = subprocess.run(f"git rev-parse origin/{base_ref}", check=True, shell=True, capture_output=True)
-
-    base_hash = base_revision.stdout.decode().strip()
+    fetch_head_only(base_ref)
+    base_hash = get_base_revision(base_ref)
 
     if head_hash == base_hash:
         print(f"HEAD identical with {base_ref}, no commits to check")
         return 0
 
-    print("Getting commit list...")
-    subprocess.run("git fetch --unshallow", check=True, shell=True)
-    log = subprocess.run(f"git log --pretty='format:%H %s' {base_hash}..{head_hash}",
-                         check=True, shell=True, capture_output=True)
-
-    commits, subject_markers, _ = zip(*(line.decode().split(maxsplit=2) for line in log.stdout.splitlines()))
+    fetch_full_history()
+    subject_markers = get_subject_markers(head_hash, base_hash)
 
     fixups, squashes = (
         sum(1 for subject_marker in subject_markers if subject_marker == marker)
@@ -32,10 +53,7 @@ def main(head_hash: str, base_ref: str) -> int:
     else:
         print("No fixups or squashes found, check passed!")
 
-    parents = subprocess.run(f"git log --pretty=%P {base_hash}..{head_hash}",
-                             check=True, shell=True, capture_output=True)
-
-    if any(len(line.split(maxsplit=1)) > 1 for line in parents.stdout.splitlines()):
+    if has_merge_commits(head_hash, base_hash):
         print("Branch contains merge commits!")
         return 1
     else:
