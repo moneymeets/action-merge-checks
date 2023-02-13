646fceb34f3edcab779b45bdca5d55ecdb8b3d72
Abiodun Sotunde <abiodun.sotunde@moneymeets.com>
2023-02-13 13:29:04 +0100

    chore(poetry): update python to 3.11

--- merge_checks/commit_status_setter.py
+++ merge_checks/commit_status_setter.py
@@ -10,7 +10,6 @@ def set_commit_status(
     description: str,
     details_url: str,
 ):
-
     github.Github(login_or_token=token).get_repo(repository).get_commit(sha=commit_sha).create_status(
         state=state,
         target_url=details_url,
